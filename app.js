faac0262c6aec799624f467ed9fdce819abaade6
Chirag Paliwal <chirag.paliwal06@gmail.com>
2018-03-07 21:17:01 +0530

    login view added

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Home Route
 app.get('/', function (req, res) {
     res.render('index', {title: 'Home'});
+});
+
+//Login Route
+app.get('/login', function (req, res) {
+    res.render('login', {title: 'Login'});
 });
