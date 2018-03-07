26fcef8a0d556d5c01ab8fce0a7b178821217993
chiragpaliwal <chirag.paliwal06@gmail.com>
2018-03-07 17:44:24 +0530

    added static files

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const ejs = require('ejs');
-const mongoose =  require('mongoose');
+const mongoose = require('mongoose');
+
+const router = require('express').Router();
 
 app.listen('3000', function(){
-    console.log("App listening on port 3000");
+    console.log('Server started on port 3000');
 });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.render('index');    
+app.use(express.static(path.join(__dirname, 'public')));
+
+router.get('/', function(req, res, next){
+    let pageTitle = 'Home from router';
+    res.render('index', {pageTitle: pageTitle});
 });
